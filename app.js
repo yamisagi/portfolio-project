c3d034ecb5f206ff5f0c4f435cdb451148c6aa5a
Yamisagi <erencandan20@gmail.com>
2023-05-18 00:13:39 +0300

    Fix: Fix Slider buttons etc.

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 $(window).resize(function () {
     if ($(window).width() < 768) {
-        $('.header').html('<h1>I am Eren</h1>');
+        $('.header').html('<h1 class="header">I am Eren</h1>');
     }
     else {
         $('.header').html('<h1 class="header">I am Eren, a <span>Flutter</span> developer.</h1>');
     }
 });
 
+// Autoplay video for safari
 
-// For video popup 2
-$('.video__popup--2').click(function () {
-    window.open('https://user-images.githubusercontent.com/84624853/191823036-7e6207d9-260e-4329-8875-265c957c8431.mp4', '_blank');
+var promise = document.querySelector('video').play();
+
+if (promise !== undefined) {
+    promise.then(_ => {
+        // Autoplay started!
+    }).catch(error => {
+    });
 }
-);
+// Fix
